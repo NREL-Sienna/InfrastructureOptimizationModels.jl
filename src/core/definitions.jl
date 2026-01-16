0d3c000a0a567b429fcb1e526f14a6eda3f4f682
Luke Kiernan <lkiernan@nrel.gov>
2026-01-16 16:15:59 -0700

    bugfix: index types swapped

--- src/core/definitions.jl
+++ src/core/definitions.jl
@@ -74,8 +74,8 @@ const UpDown = NamedTuple{(:up, :down), NTuple{2, Float64}}
 const InOut = NamedTuple{(:in, :out), NTuple{2, Float64}}
 
 # Type alias for decision model indices - used for indexing into result stores
-const DecisionModelIndexType = Int
-const EmulationModelIndexType = Dates.DateTime
+const DecisionModelIndexType = Dates.DateTime
+const EmulationModelIndexType = Int
 
 const BUILD_PROBLEMS_TIMER = TimerOutputs.TimerOutput()
 const RUN_OPERATION_MODEL_TIMER = TimerOutputs.TimerOutput()
